4ab7ea637f66cf57ab00cc9bf4c1f95cc3e1f324
Douglas Silva <dogusky.silva@outlook.com>
2021-12-16 11:18:47 -0300

    feat: adiciona estrutura para mostrar um verse

---
+++ src/components/Verse.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Verse from '@/domain/models/verse';
+
+defineProps<{ verse: Verse }>();
+</script>
+
+<template>
+  <p><span>{{verse.number}}</span> {{verse.text}}</p>
+</template>
---
+++ tests/unit/components/Verse.spec.ts
@@ -0,0 +1,16 @@
+import { shallowMount } from "@vue/test-utils"
+import Verse from '@/components/Verse.vue'
+
+describe("Verse", () => {
+  it("shoulbe verse text", () => {
+    const verse = {
+      number: 1,
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu."
+    }
+
+    const wrapper = shallowMount(Verse, { props: { verse } })
+
+    expect(wrapper.find("p").text()).toMatch(verse.text)
+    expect(wrapper.find("span").text()).toMatch(verse.number.toString())
+  })
+})
